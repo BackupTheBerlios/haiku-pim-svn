8d5ca4084bbc75f0fb6d8900faf164a008025efe
hitech <hitech@149d6d20-b877-0410-8f77-d33b85d2d39a>
2011-09-10 00:23:38 +0000

    Update of preferences

--- trunk/PreferencesPreflet/PreferencesPreflet.h
+++ trunk/PreferencesPreflet/PreferencesPreflet.h
@@ -10,6 +10,25 @@
 #include <Archivable.h>
 #include <List.h>
 #include <Message.h>
+#include <GraphicDefs.h>
+
+#include <String.h>
+#include <MenuField.h>
+#include <PopUpMenu.h>
+
+#include "CalendarModule.h"
+#include "Utilities.h"
+
+/*------------------------------------------------------------------
+ * 		Constants of the messages
+ *-----------------------------------------------------------------*/
+const uint32	kCalendarModuleChosen = 'CALM';
+const uint32	kCalendarModuleWeekendDaySelected = 'WKDS';
+
+
+/*------------------------------------------------------------------
+ * 		Preferences of an individual CalendarModule
+ *-----------------------------------------------------------------*/
 
 /*! \class 		CalendarModulePreferences
  *	\brief		These preferences define every calendar module.
@@ -19,65 +38,141 @@
  *				the week is not configurable: weekends can be
  *				different in different countries, but Gregorian
  *				Calendar defines the week as 7 days worldwide.
- */
+ *
 class CalendarModulePreferences
 	:
 	public BArchivable
 {
-private:
+protected:
 	BList weekends;	//!< List of the weekends for this module
 	uint32	uiFirstDayOfWeek;	//!< Duh
+	rgb_color	weekendsColor;	//!< Color that's associated with weekends
+	rgb_color	weekdaysColor;	//!< Color that's associated with weekdays 
+	
 	BString id;		//!< ID of the relevant calendar module
+
 public:
-	CalendarControlPreferences();
-	CalendarControlPreferences( BMessage* archive );
+	CalendarModulePreferences();
+	CalendarModulePreferences( BMessage* archive );
 	static BArchivable* Instantiate ( BMessage* archive );
 	
 	void 	AddToWeekends (uint32 day);
-	BList 	GetWeekends (void);
+	BList 	GetWeekends (void) const;
 	
 	void 	SetFirstDayOfWeek(uint32 day);
-	uint32	GetFirstDayOfWeek( void );
+	uint32	GetFirstDayOfWeek( void ) const;
 	
-	virtual status_t Archive( BMessage* archive, bool deep = true );	
+	void 	SetColor( rgb_color	color; bool weekends = true );
+	rgb_color	GetColor( bool weekends = true ) const;
+	
+	virtual status_t Archive( BMessage* archive, bool deep = true );
+	virtual bool operator== (const CalendarModulePreferences& other ) const;
+	virtual CalendarModulePreferences operator= (const CalendarModulePreferences& other);
+	
 };	// <-- end of class CalendarControlPreferences
 
+
+ *---------------------------------------------------------
+ * 		Preferences of HourMinControl
+ *-------------------------------------------------------*
 
 class HourMinControlPreferences
 	:
 	public BArchivable
 {
-private:
+protected:
 	bool bIsTime24Hours;
 	
 public:
+	HourMinControlPreferences();
+	HourMinControlPreferences( BMessage* archive );
+	static BArchivable* Instantiate ( BMessage* archive );
 	
+	virtual bool operator==( const HourMinControlPreferences& other ) const;
+	virtual HourMinControlPreferences operator= ( const HourMinControlPreferences& other);
 	
 };	// <-- end of class HourMinControlPreferences
 
 
+
+ *-----------------------------------------------------------
+ * 		Overall preferences
+ *---------------------------------------------------------*
 class Preferences
 	:
 	public BArchivable
 {
 	HourMinControlPreferences hmControlPrefs;
+	CalendarModulePreferences cModulePrefs[ NUMBER_OF_CALENDAR_MODULES ];
 	
-	
-	
+	virtual bool operator==( const Preferences& other ) const;
+	virtual Preferences operator= ( const Preferences& other );
 	
 };	// <-- end of class Preferences
 
 
-
+/*----------------------------------------------------------------------------
+ * 		Application window
+ *--------------------------------------------------------------------------*/
+ 
 class PreferencesApplicationWindow
 	:
 	public BWindow	
 {
+protected:
+	bool bPreferenesChanged;
+	
+	Preferences		savedPreferences;	//!< A copy read from the disk
+	Preferences		actualPreferences;	//!< An object edited by the user
+	
 public:
+	PreferencesApplicationWindow();
+	virtual ~PreferencesApplicationWindow();
 	
-	
-private:
-
+	void MessageReceived( BMessage *in );
 };	// <-- class PreferencesApplication
+
+
+/*--------------------------------------------------------------------------
+ *		Main view
+ *------------------------------------------------------------------------*/
+class PreferencesMainView
+	:
+	public BView
+{
+protected:
+	CalendarModulePreferencesView*	calPrefs;
+	
+
+public:
+	PreferencesMainView( BRect frame );
+	virtual ~PreferencesMainView();
+	
+	
+};	// <-- end of class PreferencesMainView
+
+
+/*----------------------------------------------------------------------------
+ *		Calendar module preferences view
+ *--------------------------------------------------------------------------*
+class CalendarModulePreferencesView
+	:
+	public BView
+{
+	protected:
+		CalendarModulePreferences prefs[ NUMBER_OF_CALENDAR_MODULES ];	//!< The subject of all settings
+		
+		BMenuField* calendarModuleSelector;
+		BPopUpMenu* calendarModules;
+		
+		BPopUpMenu*	PopulateModulesMenu();
+		BBox*	CreateWeekendSelectionBox( BRect frame, BString &label );
+		
+	public:
+		CalendarModulePreferencesView( BFrame frame, const char* name );
+		virtual ~CalendarModulePreferencesView();
+	
+};	// <-- end of class CalendarModulePreferencesView
+*/
 
 #endif //  _PREFERENCES_PREFLET_H_
